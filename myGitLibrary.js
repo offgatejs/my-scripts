80ff42c20c8927044fb85b69228a350619cd9c4d
offgatejs <offgate2026@gmail.com>
2026-07-08 11:19:40 +0400

    Add files via upload

---
+++ myGitLibrary.js
@@ -0,0 +1,9 @@
+({
+    calculateDiscount: function(basePrice, discountRate) {
+        return basePrice * (1 - discountRate);
+    },
+    
+    logHello: function() {
+        return "Hello dynamically from Git!";
+    }
+})
